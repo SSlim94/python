c2cd7d4f12ccfe53faa1996edd6faf7d9752f1a2
(s) Sam Slim <sslim@pm7-0079.uopnet.plymouth.ac.uk>
2015-10-30 13:27:17 +0000

    Python work

--- twitterFeed/tweeting.py
+++ twitterFeed/tweeting.py
@@ -1,5 +1,5 @@
-import urllib2
-import twitter, datetime
+import urllib2    #importing library for url 
+import twitter, datetime  #importing twitter library + date and time 
 
 file = open("twitterCreds.txt")
 creds = file.readline().strip().split(',')
@@ -7,23 +7,23 @@ creds = file.readline().strip().split(',')
 #response = urllib2.urlopen("http://")
 #html = response.read()
 
-currentSession=open("/Users/sslim/Library/Application Support/Google/Chrome/Default/Current Session")
+currentSession=open("/Users/sslim/Library/Application Support/Google/Chrome/Default/Current Session")  
 rawtext = currentSession.read()
 lines = rawtext.splitlines()
 
 lastURL = ""
-for line in lines:
+for line in lines: 
     if(line.find("//") != -1):
-        startIndex = line.rfind("//") + 2
+        startIndex = line.rfind("//") + 2  
         endIndex = line.rfind("/")
-        lastURL = line[startIndex:endIndex]
+        lastURL = line[startIndex:endIndex]  #finding where the // is in html code and splitting it
     
-print(lastURL)
+print(lastURL)  #priniting shorter url 
     
-#api = twitter.Api(creds[0],creds[1],creds[2],creds[3])
+api = twitter.Api(creds[0],creds[1],creds[2],creds[3])
 
-#timestamp = datetime.datetime.utcnow()
+timestamp = datetime.datetime.utcnow()
 
-#response = api.PostUpdate("Tweeted at " + str(timestamp))
+response = api.PostUpdate("I'm really liking this website " + lastURL + "and this was tweeted at " + str(timestamp))
                          
-#print("Status updated to: " + response.text)                        
+print("Status updated to: " + response.text)                        
